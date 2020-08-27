18f613020ef86b15249c09a724991c943eda129c
John Wilson <john66@talk21.com>
2020-08-27 10:03:59 +0100

    sqlite3 - return query results as an array of dicts

--- indexer.py
+++ indexer.py
@@ -24,17 +24,23 @@ CREATE TABLE IF NOT EXISTS fileindex (
 
 );
 """
-create_fileindex = """
+
+
+
+insert_fileindex = """
 INSERT INTO
   `fileindex` (`hdrive_uid`, `fname`, `path`, `hash`,`size`)
-VALUES
-  ('James', 25, 'male', 'USA'),
-  ('Leila', 32, 'female', 'France'),
-  ('Brigitte', 35, 'female', 'England'),
-  ('Mike', 40, 'male', 'Denmark'),
-  ('Elizabeth', 21, 'female', 'Canada');
+VALUES (?, ?, ?, ?, ?) ;
 """
 
+query_count_fileindex = """
+SELECT count(*) FROM `fileindex`
+"""
+
+
+query_all_fileindex = """
+SELECT id, hdrive_uid as hdid, fname as file, path, hash, size FROM `fileindex`
+"""
 
 def creation_date(path_to_file):
     """
@@ -58,6 +64,7 @@ def create_connection(path):
     connection = None
     try:
         connection = sqlite3.connect(path)
+        connection.row_factory = sqlite3.Row  #this for getting the column names!
         print("Connection to SQLite DB successful")
     except Error as e:
         print(f"The error '{e}' occurred")
@@ -74,6 +81,34 @@ def execute_query(connection, query):
         print(f"The error '{e}' occurred")
 
 
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+def insert(connection, query, params):
+    cursor = connection.cursor()
+    cursor.execute(query,params)
+    connection.commit()
+    return cursor.lastrowid
+
+
+
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return results
+    except Error as e:
+        print(f"The error '{e}' occurred")
 
 def initTables(connection):
     execute_query(connection, create_fileindex_table)
@@ -88,6 +123,25 @@ def md5(fname):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
+
+def getRecordCount():
+    return 0
+
+
+def addFileIndexRecord(connection, harddriveid, filename, pathname, hash, sz):
+    try:
+    #  `fileindex` (`hdrive_uid`, `fname`, `path`, `hash`,`size`)
+        print("addFileIndexRecord", harddriveid, filename, pathname, hash, sz)
+
+        lr = insert(connection, insert_fileindex,(harddriveid, filename, pathname, hash, sz))
+        return lr
+    except Error as e:
+        #print(f"Ignore duplicate {e}")
+        return -1
+
+
+
+# Starts Here
 
 print(f"Name of the script      : {sys.argv[0]}")
 print(f"Arguments of the script : {sys.argv[1:]}")
@@ -107,8 +161,6 @@ try:
             dbase = arg
 
     hduid = uuid.UUID(os.popen(f"diskutil info {disk}| grep 'Volume UUID'").read().split()[2])
-    print(hduid)
-    exit()
 
 except go.GetoptError as e1:
     # Print something useful
@@ -124,21 +176,13 @@ except Error as e2:
 
 
 
-if os.geteuid() >  0:
-    print("ERROR: Must be root to use")
-    sys.exit(1)
-x = os.system("wmic diskdrive get serialnumber")
-print(x)
+#if os.geteuid() >  0:
+#    print("ERROR: Must be root to use")
+#    sys.exit(1)
 
 
 
-sum = 0
-for opt, arg in opts:
-    sum += int(arg)
 
-
-print(sum)
-exit()
 
 id = uuid.uuid1()
 print ("uuid",id.hex)
@@ -153,20 +197,15 @@ connection = create_connection("/Users/johnny/.indexer.sqlite")
 initTables(connection)
 
 
-users = [
-    ("James", 25, "male", "USA"),
-    ("Leila", 32, "female", "France"),
-    ("Brigitte", 35, "female", "England"),
-    ("Mike", 40, "male", "Denmark"),
-    ("Elizabeth", 21, "female", "Canada"),
-]
 
-user_records = ", ".join(["%s"] * len(users))
-print(user_records)
-insert_query = f"INSERT INTO fileindex (hdrive_uid, path, size, hash) VALUES {user_records}"
+#user_records = ", ".join(["%s"] * len(users[0]))
+#print(user_records)
+#insert_query = f"INSERT INTO fileindex (hdrive_uid, path, size, hash) VALUES {user_records}"
 #buildQuery(insert_query,users)
 
-rootDir = '.'
+rootDir = root
+added = 0
+
 for dirName, subdirList, fileList in os.walk(rootDir, topdown=False):
     print('Found directory: %s' % dirName)
     for fname in fileList:
@@ -176,7 +215,22 @@ for dirName, subdirList, fileList in os.walk(rootDir, topdown=False):
         ctime = creation_date(ffname)
         md5h = md5(ffname)
 #        print('\t%s\t%s\t%s\t%d\t%s\t%s' % (fname, dirName, md5h, sz, time.ctime(ctime), time.ctime(mtime) ))
-        print('\t%s\t%s\t%s\t%d\t%s\t%s' % (fname, dirName, md5h, sz, ctime, mtime ))
+        #print('\t%s\t%s\t%s\t%d\t%s\t%s' % (fname, dirName, md5h, sz, ctime, mtime ))
+        #  `fileindex` (`hdrive_uid`, `fname`, `path`, `hash`,`size`)
+        lr = addFileIndexRecord(connection, disk, fname, dirName, md5h, sz)
+        if (lr > 0):
+            added = added + 1
 
         # add to database
         # uuid + ffname is unique key
+
+print("Added",added)
+results=execute_read_query(connection, query_count_fileindex)
+print("Number of Records",results[0][0])
+
+results=execute_read_query(connection, query_all_fileindex)
+
+for result in results:
+    r = dict(result)
+    #print(r)
+    print(r['id'], r['hdid'], r['file'], r['path'], r['hash'], r['size'])
