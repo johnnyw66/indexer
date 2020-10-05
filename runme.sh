9518e5443bb8e867b7566f80597d46e866ba8ea7
John Wilson <john66@talk21.com>
2020-10-05 15:09:21 +0100

    updated main script with new volumes

--- runme.sh
+++ runme.sh
@@ -7,7 +7,13 @@ diskutil info '/Volumes/MacBookAirTimeMachine/' | grep 'Volume UUID'
 #./indexer.py -h '/Volumes/Seagate Backup Plus Drive/' -r '/Volumes/Seagate Backup Plus Drive/' -d '/Users/johnny/.indexer.sqlite' -n mike -b 32768 --scanning
 #./indexer.py -h '/Volumes/Seagate Backup Plus Drive/' -r '/Volumes/Seagate Backup Plus Drive/SQLITE/' -d '/Users/johnny/.indexer.sqlite' -n mike -b 32768 --scanning
 #./indexer.py -h '/Volumes/MacBookAirTimeMachine/' -r '/Volumes/MacBookAirTimeMachine/' -d '/Users/johnny/.indexer.sqlite' -n sue -b 32768 --scanning
-./indexer.py -h '/Volumes/Untitled/' -r '/Volumes/Untitled/' -d '/Users/johnny/.indexer.sqlite' -n lizzy -b 32768 --scanning
+#./indexer.py -h '/Volumes/Untitled/' -r '/Volumes/Untitled/' -d '/Users/johnny/.indexer.sqlite' -n lizzy -b 32768 --scanning
+#./indexer.py -h '/Volumes/Untitled/' -r '/Volumes/Untitled/' -d '/Users/johnny/.indexer.sqlite' -n joan -b 32768 --scanning
+#./indexer.py -h '/Volumes/Untitled/' -r '/Volumes/Untitled 1/' -d '/Users/johnny/.indexer.sqlite' -n joan -b 32768 --scanning
+
+#./indexer.py -h '/Volumes/macosx/' -r '/Volumes/macosx/' -d '/Users/johnny/.indexer.sqlite' -n pedro -b 32768 --scanning
+#./indexer.py -h '/Volumes/BOOTCAMP/' -r '/Volumes/BOOTCAMP/' -d '/Users/johnny/.indexer.sqlite' -n charlie -b 32768 --scanning
+
 
 ./indexer.py  -d '/Users/johnny/.indexer.sqlite' --report
 #./email.py -m 'Indexing Completed' -t 'johnnyw66@gmail.com' -s 'Indexing Job Complete'
