c8d751e76d134e33a973ab72576c25ec5e28f62f
John Wilson <john66@talk21.com>
2021-02-10 23:30:33 +0000

    correct di.sh tool (used for finding drives uuid)

--- di.sh
+++ di.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 volume=$1
-diskutil info $volume | grep 'Volume UUID'
-shift
-echo $@
-
-#echo "Hello, world $v!"
-#echo $v
-
-#v = 'hello world'
-#shift
-#echo $v
-#echo $@
+echo "$volume"
+diskutil info "$volume" | grep 'Volume UUID'
